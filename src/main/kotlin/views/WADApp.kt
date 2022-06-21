635f70dfa51f119f27823038e5bfbefb8fe0c721
pwshady <pwshady@gmail.com>
2022-06-21 23:35:53 +0300

    21-06

--- src/main/kotlin/views/WADApp.kt
+++ src/main/kotlin/views/WADApp.kt
@@ -3,7 +3,8 @@ package views
 import javafx.stage.Stage
 import tornadofx.App
 
-class WADApp() : App(WADProjectsView::class)  {
+class WADApp() : App(WADProjectsView::class)
+{
     override fun start(stage: Stage){
         with(stage){
             isResizable = false
